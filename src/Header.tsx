8cd6b7e770f3b501ae6899b7d1f3b3610b701a18
LayneHaber <layne.haber@gmail.com>
2019-12-09 22:56:47 -0800

    prettier + router

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,52 +1,76 @@
-import React, { useContext } from 'react';
-import { Notifications, notificationsMachine } from './Notifications';
-import { StyledHeader, StyledLogo, StyledLinks, StyledLink } from './App';
-import { interpret } from 'xstate';
-import { Actor } from 'xstate/lib/Actor';
+import React from "react";
+import { notificationsMachine } from "./Notifications";
+import { StyledHeader, StyledLinks, StyledLink } from "./App";
+import { interpret } from "xstate";
+import { Actor } from "xstate/lib/Actor";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Notification } from './Notifications';
+import { Notification } from "./Notifications";
+import {
+  Deposit,
+  Install,
+  InstallVirtual,
+  Propose,
+  Uninstall,
+  UninstallVirtual,
+  Withdraw
+} from "./machines";
 
 const notificationsService = interpret(notificationsMachine).start();
 
 export const notificationsActor: Actor & {
   notify: (message: string | Notification) => void;
 } = {
-  toJSON: () => ({ id: 'notifications' }),
-  id: 'notifications',
+  toJSON: () => ({ id: "notifications" }),
+  id: "notifications",
   send: notificationsService.send.bind(notificationsService),
   subscribe: notificationsService.subscribe.bind(notificationsService),
   notify: (message: string | Notification) =>
     notificationsService.send({
-      type: 'NOTIFICATIONS.QUEUE',
+      type: "NOTIFICATIONS.QUEUE",
       data:
-        typeof message === 'string' ? { message, severity: 'info' } : message
+        typeof message === "string" ? { message, severity: "info" } : message
     })
 };
 
 export function Header() {
   return (
-    <StyledHeader>
-      <StyledLogo />
-      <StyledLinks>
-        <StyledLink
-          href="https://github.com/davidkpiano/xstate"
-          target="_xstate-github"
-        >
-          GitHub
-        </StyledLink>
-        <StyledLink href="https://xstate.js.org/docs" target="_xstate-docs">
-          Docs
-        </StyledLink>
-        <StyledLink
-          href="https://spectrum.chat/statecharts"
-          target="_statecharts-community"
-        >
-          Community
-        </StyledLink>
-        <StyledLink href="https://opencollective.com/xstate" target="_sponsor">
-          Sponsor 💙
-        </StyledLink>
-      </StyledLinks>
-    </StyledHeader>
+    <Router>
+      <StyledHeader>
+        <StyledLinks>
+          <StyledLink to="/deposit">Deposit</StyledLink>
+          <StyledLink to="/propose">Propose</StyledLink>
+          <StyledLink to="/install">Install</StyledLink>
+          <StyledLink to="/uninstall">Uninstall</StyledLink>
+          <StyledLink to="/installvirtual">Install Virtual</StyledLink>
+          <StyledLink to="/uninstallvirtual">Uninstall Virtual</StyledLink>
+          <StyledLink to="/withdraw">Withdraw</StyledLink>
+        </StyledLinks>
+      </StyledHeader>
+
+      <Switch>
+        <Route path="/deposit">
+          <Deposit />
+        </Route>
+        <Route path="/propose">
+          <Propose />
+        </Route>
+        <Route path="/install">
+          <Install />
+        </Route>
+        <Route path="/uninstall">
+          <Uninstall />
+        </Route>
+        <Route path="/installvirtual">
+          <InstallVirtual />
+        </Route>
+        <Route path="/uninstallvirtual">
+          <UninstallVirtual />
+        </Route>
+        <Route path="/withdraw">
+          <Withdraw />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
