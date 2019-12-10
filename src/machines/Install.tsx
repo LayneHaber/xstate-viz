aad9bb5ccee1aba8c57910c48523240fbd141b4f
LayneHaber <layne.haber@gmail.com>
2019-12-10 00:36:54 -0800

    init commit install and propose

--- src/machines/Install.tsx
+++ src/machines/Install.tsx
@@ -3,6 +3,7 @@ import { Machine } from "xstate";
 
 import { ProtocolContext } from "../utils/channelTypes";
 import { StateChart } from "..";
+import { uniqueId } from "xstate/lib/utils";
 
 const installMachine = Machine<ProtocolContext>(
   {
@@ -17,7 +18,70 @@ const installMachine = Machine<ProtocolContext>(
     },
     initial: "proposed",
     states: {
-      proposed: {},
+      proposed: {
+        on: {
+          protocolInitiated: "inProtocol"
+        }
+      },
+      inProtocol: {
+        type: "parrallel",
+        initial: "initiated",
+        states: {
+          // initiating party in protocol
+          initiator: {
+            type: "compound",
+            states: {
+              persistChannel: {
+                on: {
+                  PERSIST_STATE_CHANNEL: 'freeBalanceUpdate'
+                }
+              },
+              freeBalanceUpdate: {
+                type: "final",
+                states: {
+                  // sign message
+                  // send data
+                  // will timeout
+                  sendSignature: {
+                    on: {
+                      TIMEOUT: 'failure.timeout',
+                      REJECT: 'failure'
+                    }
+                  }
+                },
+              }
+            }
+          },
+          // responding party in protocol
+          responding: {
+            type: "compound",
+            states: {}
+          },
+          failure: {
+            type: "final",
+            initial: "rejection",
+            states: {
+              rejection: {
+                meta: {
+                  message: "Install rejected"
+                }
+              },
+              timeout: {
+                meta: {
+                  message: "Install timed out"
+                }
+              }
+            },
+            meta: { message: "Install failed" }
+          },
+        }
+      },
+      installed: {
+        type: "final",
+        meta: {
+          installedApps: 1,
+        }
+      },
     }
   },
   {
--- src/machines/Propose.tsx
+++ src/machines/Propose.tsx
@@ -4,6 +4,25 @@ import { Machine } from "xstate";
 import { ProtocolContext } from "../utils/channelTypes";
 import { StateChart } from "..";
 
+const agentProtocolFailureEvents = {
+  BAD_SIG: {
+    target: "failure.badSignature",
+    internal: false,
+  },
+  PROPOSAL_ACCEPTED: {
+    target: "proposed",
+    internal: false,
+  },
+  TIMEOUT: {
+    target: "failure.timeout",
+    internal: false
+  },
+  WRITE_FAIL: {
+    target: "failure.protocolFailure",
+    internal: false
+  }
+}
+
 const proposeMachine = Machine<ProtocolContext>(
   {
     id: "propose",
@@ -15,10 +34,68 @@ const proposeMachine = Machine<ProtocolContext>(
         currentMessage: undefined
       }
     },
-    initial: "noProposal",
+    initial: "initializing",
     states: {
-      noProposal: {},
-      proposalInitiated: {}
+      initializing: {
+        type: "atomic",
+        on: {
+          PROPOSE_CALLED: "initiator"
+        },
+        states: {}
+      },
+      initiator: {
+        type: "compound",
+        on: { ...agentProtocolFailureEvents },
+        states: {
+          getStateChannel: {},
+          addProposal: {},
+          persistChannel: {},
+          generateCommitment: {},
+          signCommitment: {},
+          sendToResponder: {},
+          recieveSignature: {},
+          updateMapping: {},
+        }
+      },
+      responder: {
+        type: "compound",
+        on: { ...agentProtocolFailureEvents },
+        states: {
+          recieveMessage: {},
+          getStateChannel: {},
+          generateCommitment: {},
+          assertSignature: {},
+          addProposal: {},
+          persistChannel: {},
+          signCommitment: {},
+          sendMessage: {},
+        }
+      },
+      // protocol has finished for both participants with
+      // channels updated, leaves the connext context
+      proposed: {
+        // type: ,
+        on: {
+          EVENT_FIRED: "notification"
+        },
+        states: {}
+      },
+      // protocol has failed
+      failure: {
+        type: "final",
+        states: {
+          timeout: {},
+          rejected: {},
+          badSignature: {},
+          protocolFailure: {}
+        }
+      },
+      notification: {
+        type: "final",
+        states: {
+          connextEventEmitted: {}
+        }
+      }
     }
   },
   {
